8418eebbf4a5689cbc8cd6edfc471603242164f5
dongjinkimbig <117065088+dongjinkimbig@users.noreply.github.com>
2022-12-28 11:46:42 +0900

    Revert "#8-스프링부트 프로젝트 시작하기"

--- project-board/src/main/java/com/koreait/projectboard/repository/ArticleCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.koreait.projectboard.repository;
-
-import com.koreait.projectboard.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-    
-}
